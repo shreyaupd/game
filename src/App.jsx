404ef0abf98ec62674708bd21d36bd42d80695a3
shreyaupd <shreyaupd234@gmail.com>
2025-06-25 23:07:17 +0545

    Feature component ...

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
+import Feature from './components/Feature'
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Navbar/>
       <Hero />
       <About/>
+      <Feature/>
     </main>
   )
 }
---
+++ src/components/Feature.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Feature = () => {
+  return (
+    <section className='bg-black pb-52'>
+        <div className='flex flex-col pt-24 pl-20'>
+            <p className='text-white font-circular-web'>
+                Into The Metagame Layer
+            </p>
+            <p className=' max-w-96 font-circular-web text-lg text-blue-50 opacity-50 '>
+                Immerse yourself in an IP-rich product universe where players, agentic AI and blockchain lead the new economic paradigm.
+            </p>
+             
+        </div>
+
+
+
+         <div>
+            
+         </div>
+    </section>
+  )
+}
+
+export default Feature
