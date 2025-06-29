91fce3758e5b56f4d4fe9c31d4d201523da9a480
shreyaupd <shreyaupd234@gmail.com>
2025-06-29 14:34:20 +0545

    Story component for improved readability and maintainability

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -8,21 +8,21 @@ import { useState } from "react";
 
 const Story = () => {
   const imgref = useRef();
-const handleMouseMove = (e) => {
+  const handleMouseMove = (e) => {
     console.log(e);
     const { clientX, clientY } = e;
-    const element= imgref.current;
-    if(!element)
+    const element = imgref.current;
+    if (!element)
       return;
-    const rect =  element.getBoundingClientRect()
-    const xpos =clientX-rect.left;
-    const ypos =clientY-rect.top;
-    const centerX=rect.width/2;
-    const centerY=rect.height/2;
-    const rotateX=((ypos-centerY)/centerY)*-10
-    const rotateY=((xpos-centerX)/centerX)*10
+    const rect = element.getBoundingClientRect()
+    const xpos = clientX - rect.left;
+    const ypos = clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    const rotateX = ((ypos - centerY) / centerY) * -10
+    const rotateY = ((xpos - centerX) / centerX) * 10
 
-     gsap.to(element, {
+    gsap.to(element, {
       duration: 0.3,
       rotateX,
       rotateY,
@@ -33,12 +33,12 @@ const handleMouseMove = (e) => {
 
   const handleMouseLeave = () => {
     const element = imgref.current;
-    if(element){
+    if (element) {
       gsap.to(element, {
-      rotateX: 0,
-      rotateY: 0,
-      ease: "power1.inOut",
-    });
+        rotateX: 0,
+        rotateY: 0,
+        ease: "power1.inOut",
+      });
     }
   }
   return (
@@ -49,27 +49,45 @@ const handleMouseMove = (e) => {
         </p>
 
         <div className="relative size-full">
-        <AnimatedTitle
-        title="the st<b>o</b>ry of <br/> a hidden real<b>m</b>"
-        containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
-        />
-         <div className="story-img-container">
-          <div className="story-img-mask">
-            <div className="story-img-content">
-              <img src="/img/entrance.webp"
-              ref={imgref}
-              onMouseMove={handleMouseMove}
-               onMouseEnter={handleMouseLeave}
-              onMouseLeave={handleMouseLeave}
-              onMouseUp={handleMouseLeave}
-              className="rounded-2xl"
-              alt="entrance.webp" />
-            </div>
+          <AnimatedTitle
+            title="the st<b>o</b>ry of <br/> a hidden real<b>m</b>"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img src="/img/entrance.webp"
+                  ref={imgref}
+                  onMouseMove={handleMouseMove}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  className="rounded-2xl"
+                  alt="entrance.webp" />
+              </div>
 
-            
+
+            </div>
           </div>
-         </div>
-          
+
+        </div>
+
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+            <a href="#_" class="relative mt-2 px-5 py-3 overflow-hidden font-medium text-gray-600 rounded-2xl bg-gray-300 border border-gray-100 rounded-lg shadow-inner group">
+              <span class="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
+              <span class="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
+              <span class="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+              <span class="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+              <span class="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
+              <span class="relative transition-colors duration-300 delay-200 group-hover:text-white ease uppercase">discover prologue</span>
+            </a>
+          </div>
         </div>
       </div>
     </div>
